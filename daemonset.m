91d635cd76f0c46fb81be5c28af50def0860684b
Arunkumar-1129 <arunkumar.p2024ece@sece.ac.in>
2026-06-02 10:46:40 +0530

    Deploy nginx replicaset in new 'dev' namespace
    Created a new namespace 'dev' and deployed a replicaset 'nginx-rs' with 3 replicas. Updated pod management commands for nginx daemonset.

--- daemonset.m
+++ daemonset.m
@@ -76,3 +76,67 @@ pod/nginx-deployment-86c8cd48f6-qpw5h   1/1     Running   1 (50m ago)   11h     
 pod/nginx-pod                           1/1     Running   1 (50m ago)   12h     10.42.1.47   k3s-worker2.sece.com   <none>           <none>
 
 
+
+kubectl delete pod/nginx-daemonset-w2rxg
+
+
+creating namespace
+
+root@k3s-master:~# vi nginx-rs.yaml
+root@k3s-master:~# kubectl create ns dev
+namespace/dev created
+root@k3s-master:~# kubectl apply -f nginx-rs.yaml
+replicaset.apps/nginx-rs created
+root@k3s-master:~# kubectl get rs -n dev
+NAME       DESIRED   CURRENT   READY   AGE
+nginx-rs   3         3         0       10s
+root@k3s-master:~# kubectl get pods -A
+NAMESPACE     NAME                                      READY   STATUS      RESTARTS       AGE
+default       nginx-daemonset-7t6qp                     1/1     Running     0              7m42s
+default       nginx-daemonset-nl9v8                     1/1     Running     0              43m
+default       nginx-daemonset-xr74t                     1/1     Running     0              43m
+default       nginx-deployment-86c8cd48f6-2q9df         1/1     Running     1 (86m ago)    11h
+default       nginx-deployment-86c8cd48f6-4lfhk         1/1     Running     1 (86m ago)    11h
+default       nginx-deployment-86c8cd48f6-64vqq         1/1     Running     1 (87m ago)    11h
+default       nginx-deployment-86c8cd48f6-clzgq         1/1     Running     1 (86m ago)    11h
+default       nginx-deployment-86c8cd48f6-f8kt5         1/1     Running     1 (86m ago)    11h
+default       nginx-deployment-86c8cd48f6-g4nj9         1/1     Running     1 (87m ago)    11h
+default       nginx-deployment-86c8cd48f6-gff2q         1/1     Running     1 (87m ago)    11h
+default       nginx-deployment-86c8cd48f6-grhdx         1/1     Running     1 (86m ago)    11h
+default       nginx-deployment-86c8cd48f6-ngm7g         1/1     Running     1 (86m ago)    11h
+default       nginx-deployment-86c8cd48f6-qpw5h         1/1     Running     1 (86m ago)    11h
+default       nginx-pod                                 1/1     Running     1 (86m ago)    12h
+dev           nginx-rs-n5852                            1/1     Running     0              51s
+dev           nginx-rs-qp66w                            1/1     Running     0              51s
+dev           nginx-rs-zv9h6                            1/1     Running     0              51s
+kube-system   coredns-8db54c48d-v6ck2                   1/1     Running     4 (87m ago)    2d19h
+kube-system   helm-install-traefik-crd-rhl5l            0/1     Completed   0              2d19h
+kube-system   helm-install-traefik-hgnt2                0/1     Completed   2              2d19h
+kube-system   local-path-provisioner-5d9d9885bc-d9ws6   1/1     Running     4 (87m ago)    2d19h
+kube-system   metrics-server-786d997795-mljbj           1/1     Running     4 (87m ago)    2d19h
+kube-system   svclb-traefik-56ac0f3b-7pm7z              2/2     Running     8 (87m ago)    2d19h
+kube-system   svclb-traefik-56ac0f3b-gz5x8              2/2     Running     6 (86m ago)    35h
+kube-system   svclb-traefik-56ac0f3b-jt47t              2/2     Running     10 (86m ago)   2d1h
+kube-system   traefik-9bcdbbd9-ztprx                    1/1     Running     4 (87m ago)    2d19h
+root@k3s-master:~# kubectl get pods
+NAME                                READY   STATUS    RESTARTS      AGE
+nginx-daemonset-7t6qp               1/1     Running   0             8m37s
+nginx-daemonset-nl9v8               1/1     Running   0             44m
+nginx-daemonset-xr74t               1/1     Running   0             44m
+nginx-deployment-86c8cd48f6-2q9df   1/1     Running   1 (87m ago)   11h
+nginx-deployment-86c8cd48f6-4lfhk   1/1     Running   1 (87m ago)   11h
+nginx-deployment-86c8cd48f6-64vqq   1/1     Running   1 (88m ago)   11h
+nginx-deployment-86c8cd48f6-clzgq   1/1     Running   1 (87m ago)   11h
+nginx-deployment-86c8cd48f6-f8kt5   1/1     Running   1 (87m ago)   11h
+nginx-deployment-86c8cd48f6-g4nj9   1/1     Running   1 (88m ago)   11h
+nginx-deployment-86c8cd48f6-gff2q   1/1     Running   1 (88m ago)   11h
+nginx-deployment-86c8cd48f6-grhdx   1/1     Running   1 (87m ago)   11h
+nginx-deployment-86c8cd48f6-ngm7g   1/1     Running   1 (87m ago)   11h
+nginx-deployment-86c8cd48f6-qpw5h   1/1     Running   1 (87m ago)   11h
+nginx-pod                           1/1     Running   1 (87m ago)   12h
+root@k3s-master:~# kubectl get pod -n dev
+NAME             READY   STATUS    RESTARTS   AGE
+nginx-rs-n5852   1/1     Running   0          2m14s
+nginx-rs-qp66w   1/1     Running   0          2m14s
+nginx-rs-zv9h6   1/1     Running   0          2m14s
+
